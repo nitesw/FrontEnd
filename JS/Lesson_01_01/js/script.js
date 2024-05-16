9fd7a2be5ad2f55abfefa1ff38bff019ac2ae2b5
nitesw <apedos2007@gmail.com>
2024-05-16 19:26:11 +0300

    Add new javascript lessons

---
+++ JS/Lesson_01_01/js/script.js
@@ -0,0 +1,30 @@
+// Task 2
+// let num1 = parseFloat(prompt("Enter first num: "));
+// let num2 = parseFloat(prompt("Enter second num: "));
+
+// if (!isNaN(num1) && !isNaN(num2)) {
+//   let res = parseFloat((num1 + num2) / 2);
+//   console.log(`Avg of ${num1} and ${num2} is ${res}`);
+// } else {
+//   console.log("Error!");
+// }
+
+// Task 4
+// const mile = 0.621371;
+// let km = parseFloat(prompt("Enter length in km: "));
+
+// if (!isNaN(km) && km > 0) {
+//   console.log(`${km} km is ${km * mile} miles`);
+// } else {
+//   console.log("Error!");
+// }
+
+// Task 5
+let num1 = parseFloat(prompt("Enter first num: "));
+let num2 = parseFloat(prompt("Enter second num: "));
+
+console.log(
+  `${num1} + ${num2} = ${num1 + num2}\n${num1} - ${num2} = ${
+    num1 - num2
+  }\n${num1} * ${num2} = ${num1 * num2}\n${num1} / ${num2} = ${num1 / num2}`
+);
