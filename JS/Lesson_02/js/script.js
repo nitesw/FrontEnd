5182eae32d281230cc55e2bd6a197e50bdcca71b
nitesw <apedos2007@gmail.com>
2024-05-17 22:38:05 +0300

    Add new completed tasks

---
+++ JS/Lesson_02/js/script.js
@@ -0,0 +1,22 @@
+// let Sum = function (a, b) {
+//   return a + b;
+// };
+
+// let Sum = (a, b) => a + b;
+// let Sum = (a, b) => {
+//   return a + b;
+// };
+
+// let num1 = parseFloat(prompt("Enter the first number: "));
+// let num2 = parseFloat(prompt("Enter the second number: "));
+// let res = Sum(num1, num2);
+
+// console.log(`Result = ${res}`);
+
+// function Sum(a, b) {
+//   return a + b;
+// }
+
+((a, b) => {
+  console.log(`${a} + ${b} = ${a + b}`);
+})(4, 5);
