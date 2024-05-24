a90f7a75027b285ff600ed37311eadfde6ca3d38
nitesw <apedos2007@gmail.com>
2024-05-24 20:50:39 +0300

    Create Swapi table

--- JS/Lesson_05_01/js/script.js
+++ JS/Lesson_05_01/js/script.js
@@ -13,23 +13,18 @@ const GetApiData = (url, callback) => {
 };
 
 let root = document.getElementById("root");
-root.setAttribute("class", "container");
 
 let CreateCurrencyTable = (data) => {
-  //   const apiData = GetApiData(url);
-  //   apiData.then((data) => console.log(data));
+  let divContainer = document.createElement("div");
+  divContainer.setAttribute("class", "container");
 
   let divRow = document.createElement("div");
   divRow.setAttribute("class", "row");
-
-  let divCurrency = document.createElement("div");
-  divCurrency.setAttribute("class", "currency");
 
   let tableCurrency = document.createElement("table");
   tableCurrency.setAttribute("class", "table");
 
   let tableRowForHeaders = document.createElement("tr");
-  tableRowForHeaders.setAttribute("class", "tr-currency");
 
   let tableHeaderCurrency = document.createElement("th");
   tableHeaderCurrency.innerText = "Currency";
@@ -41,9 +36,12 @@ let CreateCurrencyTable = (data) => {
   tableHeaderSell.innerText = "Sell";
   tableHeaderSell.setAttribute("class", "th-currency");
 
-  root.appendChild(divRow);
+  root.appendChild(divContainer);
+  divContainer.appendChild(divRow);
   divRow.appendChild(tableCurrency);
+
   tableCurrency.appendChild(tableRowForHeaders);
+
   tableRowForHeaders.appendChild(tableHeaderCurrency);
   tableRowForHeaders.appendChild(tableHeaderBuy);
   tableRowForHeaders.appendChild(tableHeaderSell);
@@ -72,5 +70,109 @@ let CreateCurrencyTable = (data) => {
   });
 };
 
+let CreateSwapiTable = (data) => {
+  let divContainer = document.createElement("div");
+  divContainer.setAttribute("class", "container");
+
+  let divRow = document.createElement("div");
+  divRow.setAttribute("class", "row");
+
+  let tableSwapi = document.createElement("table");
+  tableSwapi.setAttribute("class", "table");
+  tableSwapi.setAttribute("class", "swapi");
+
+  let trForSwapiHeaders = document.createElement("tr");
+
+  let thName = document.createElement("th");
+  thName.setAttribute("class", "th-swapi");
+  thName.innerText = "Name";
+  let thHeight = document.createElement("th");
+  thHeight.setAttribute("class", "th-swapi");
+  thHeight.innerText = "Height";
+  let thMass = document.createElement("th");
+  thMass.setAttribute("class", "th-swapi");
+  thMass.innerText = "Mass";
+  let thHair = document.createElement("th");
+  thHair.setAttribute("class", "th-swapi");
+  thHair.innerText = "Hair";
+  let thSkin = document.createElement("th");
+  thSkin.setAttribute("class", "th-swapi");
+  thSkin.innerText = "Skin";
+  let thEyeColor = document.createElement("th");
+  thEyeColor.setAttribute("class", "th-swapi");
+  thEyeColor.innerText = "Eye color";
+  let thBirthYear = document.createElement("th");
+  thBirthYear.setAttribute("class", "th-swapi");
+  thBirthYear.innerText = "Birth year";
+  let thGender = document.createElement("th");
+  thGender.setAttribute("class", "th-swapi");
+  thGender.innerText = "Gender";
+
+  root.appendChild(divContainer);
+  divContainer.appendChild(divRow);
+  divRow.appendChild(tableSwapi);
+
+  tableSwapi.appendChild(trForSwapiHeaders);
+
+  trForSwapiHeaders.appendChild(thName);
+  trForSwapiHeaders.appendChild(thHeight);
+  trForSwapiHeaders.appendChild(thMass);
+  trForSwapiHeaders.appendChild(thHair);
+  trForSwapiHeaders.appendChild(thSkin);
+  trForSwapiHeaders.appendChild(thEyeColor);
+  trForSwapiHeaders.appendChild(thBirthYear);
+  trForSwapiHeaders.appendChild(thGender);
+
+  data.results.forEach((person) => {
+    let trPerson = document.createElement("tr");
+
+    let rowTdName = document.createElement("td");
+    rowTdName.setAttribute("class", "td-swapi-name");
+    rowTdName.innerText = person.name;
+
+    let rowTdHeight = document.createElement("td");
+    rowTdHeight.setAttribute("class", "td-swapi");
+    rowTdHeight.innerText = person.height;
+
+    let rowTdMass = document.createElement("td");
+    rowTdMass.setAttribute("class", "td-swapi");
+    rowTdMass.innerText = person.mass;
+
+    let rowTdHair = document.createElement("td");
+    rowTdHair.setAttribute("class", "td-swapi");
+    rowTdHair.innerText = person.hair_color;
+
+    let rowTdSkin = document.createElement("td");
+    rowTdSkin.setAttribute("class", "td-swapi");
+    rowTdSkin.innerText = person.skin_color;
+
+    let rowTdEyeColor = document.createElement("td");
+    rowTdEyeColor.setAttribute("class", "td-swapi");
+    rowTdEyeColor.innerText = person.eye_color;
+
+    let rowTdBirthYear = document.createElement("td");
+    rowTdBirthYear.setAttribute("class", "td-swapi");
+    rowTdBirthYear.innerText = person.birth_year;
+
+    let rowTdGender = document.createElement("td");
+    rowTdGender.setAttribute("class", "td-swapi");
+    rowTdGender.innerText = person.gender;
+
+    trPerson.appendChild(rowTdName);
+    trPerson.appendChild(rowTdHeight);
+    trPerson.appendChild(rowTdMass);
+    trPerson.appendChild(rowTdHair);
+    trPerson.appendChild(rowTdSkin);
+    trPerson.appendChild(rowTdEyeColor);
+    trPerson.appendChild(rowTdBirthYear);
+    trPerson.appendChild(rowTdGender);
+
+    tableSwapi.appendChild(trPerson);
+  });
+};
+
 const URL1 = "https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5";
 GetApiData(URL1, CreateCurrencyTable);
+
+const URL2 = "https://swapi.dev/api/people";
+GetApiData(URL2, CreateSwapiTable);
